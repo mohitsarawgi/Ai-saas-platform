e01b113f1acd6b6f79244dec2df5aecee83f40cc
mohitsarawgi <mohit9632147@gmail.com>
2024-05-06 23:54:37 +0530

    database check

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import Sidebar from '@/components/shared/Sidebar'
+import React from 'react'
+import MobileNav from '@/components/shared/MobileNav'
+
+const Layout = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <main className='root'>
+
+       <Sidebar />
+       <MobileNav />
+
+      <div className='root-container'>
+        <div className='wrapper'>
+            {children}
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Layout
---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+
+const Home = () => {
+  return (
+    <div>
+  
+    </div>
+  )
+}
+
+export default Home
